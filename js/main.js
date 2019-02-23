ebb4951acf534f27d0aaaec302a1c03429ba5173
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2019-02-23 01:39:42 +0100

    Create header and small devices navigation

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
-$(document).ready(function () {
-      
-    // EASE SCROLL
+$(document).ready(function() {
+  const hamburger = $('.hamburger');
+  hamburger.click(function() {
+    $(this).toggleClass('is-active');
+  });
+  // // EASE SCROLL
 
-    $(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 800);
-    });
-    
-    
-    //ANIMATION 
-    
-    function animation() {
-        var windowHight = $(window).height();
-        var scroll = $(window).scrollTop();
-        $('.animation').each(function () {
-            var position = $(this).offset().top;
-            var animation = $(this).attr('data-animation');
-            //var delay = $(this).attr('data-delay');
-            if (position < scroll + windowHight - 60) {
-                $(this).addClass(animation);
-                //$(this).css('animation-delay', delay);
-            }
-        });
-    }//end of animation function
+  // $(document).on('click', 'a[href^="#"]', function (event) {
+  //     event.preventDefault();
+  //     $('html, body').animate({
+  //         scrollTop: $($.attr(this, 'href')).offset().top
+  //     }, 800);
+  // });
 
-    animation();
+  // //ANIMATION
 
-    $(window).scroll(function () {
-        animation();
-    });
+  // function animation() {
+  //     var windowHight = $(window).height();
+  //     var scroll = $(window).scrollTop();
+  //     $('.animation').each(function () {
+  //         var position = $(this).offset().top;
+  //         var animation = $(this).attr('data-animation');
+  //         //var delay = $(this).attr('data-delay');
+  //         if (position < scroll + windowHight - 60) {
+  //             $(this).addClass(animation);
+  //             //$(this).css('animation-delay', delay);
+  //         }
+  //     });
+  // }//end of animation function
 
-});//end document.ready
+  // animation();
 
-
+  // $(window).scroll(function () {
+  //     animation();
+  // });
+}); //end document.ready
